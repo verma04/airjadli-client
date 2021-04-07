e8d439139aa569110ad730b1e0950301cbea9c9f
Pankaj <pankaj.k@amakein.com>
2021-04-07 21:15:23 +0530

    dd

--- componets/layout/Training/Section/Section.js
+++ componets/layout/Training/Section/Section.js
@@ -6,7 +6,7 @@ import { Section} from './Style'
 import Image from 'next/image';
 
 
-export default function Home() {
+export default function Home({data}) {
   return (
     <>
       <Head>
@@ -17,26 +17,15 @@ export default function Home() {
     <div class="grid"   >
 
         <div className="head" >
-            <h2>AirJaldi provides unlimited internet access to both individual and
-business customers. We offer a variety of monthly plans to connect
-your home or office at broadband speeds wirelessly. Our support
-team is standing by to help determine which plan is best for you.</h2>
+            <h2>{data.tranningData}</h2>
         </div>
     
 
     <div className="data" > 
     <div className="set" >
-     <h2>GET A WIRELESS INTERNET CONNECTION
-FOR YOUR HOME OR OFFICE</h2>
+     <h2>{data.SectionHead}</h2>
 
-<p>Erum volorec epudae. Os delit alibusam, si core vellaborum est, et lab id quatis consequo volo-
-res tenduci enturia dolorporem nimolum voluptiunt. Bus dolestrum quos doluptat qui beaquam
-rehentum, nos maionsequis si rest eum a sam la dicium rae sam velenda neceat explicae dolorum
-fugit, tem autectet, ut lab ium quame estiosa autati cume id qui berfere prem suntibu sapicim co-
-ribus volupta cuptaectinus quis abo. Saepe soluptium elignat am, ea qui doluptaquid et a volorio
-odignis ut od entiissi nimi, core volupta temporit aspereriandi derciate inctatur, qui as accaesto ea
-nonsequasit ra accatus amusame vendebitiae vel is incturi amendit qui con nus dolorendi offici
-beratecerum litate vent illa ento cum estinture sequi tota conseniendi re</p>
+<p>{data.SectionData}</p>
 
 <p>Call 1-800-200-9989 or email support@airjaldi.com to get connected</p>
 
@@ -49,7 +38,7 @@ beratecerum litate vent illa ento cum estinture sequi tota conseniendi re</p>
 <div className="right" >
     <Image
               className="myImage"
-        src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1613190579/AirJaldi/trgfw7joefexkkiahgrw.jpg'
+        src={data.Section2img}
        
         layout="fill"
        
@@ -61,12 +50,8 @@ beratecerum litate vent illa ento cum estinture sequi tota conseniendi re</p>
     <div className="left" >
 
 
-<p>“Our village is in a very remote place
-adn we didn’t have any internet
-facilities before AirJaldi arrived here.
-Now, we no longer need to commute
-70-80 Km to get connected.”</p>
-<span>Piyush Manviya, Private User, Katkumbh, Maharashtra</span>
+<p>“{data.Section2Data}”</p>
+<span>{data.Section2Name}</span>
 
 
 </div>
--- componets/layout/Training/Training.js
+++ componets/layout/Training/Training.js
@@ -4,13 +4,22 @@ import Footer from '../Footer/Footer'
 import { Section } from './Style'
 import NewsLetter from '../NewsLetter/News'
 import Section1 from './Section/Section'
-
+import { useQuery } from "react-query";
 import Image from 'next/image';
+import Loading from '../../Loading/Loading';
 
-
+const fetchTrianing = async () => {
+  const res = await fetch("https://airjadli.herokuapp.com/api/client/getTraining");
+  return res.json();
+};
 export default function Home() {
+  const { data, status } = useQuery("traning", fetchTrianing);
   return (
     <>
+    {status === "error" && <p>Error fetching data</p>}
+      {status === "loading" && <Loading/>}
+      {status === "success" && (
+          <>
       <Head>
         <title>Home - AirJaldi</title>
       </Head>
@@ -23,7 +32,7 @@ export default function Home() {
    </div>
     <Image
               className="myImage"
-        src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1613190579/AirJaldi/jjjcclcaox85o1dyr8ss.jpg'
+        src={data.tranningAvatar}
        
         layout="fill"
        
@@ -35,11 +44,7 @@ export default function Home() {
   <div class='grid1' >
 
   <h1>
-  Anduntias experat. Millabor rem et re
-nost, aceperuptam apere quis doluptur,
-odissunt, quo oditiore elibus esent rem.
-Ut et, cupis exerciisquo videseque nis est
-que sunto occuptatur sita volut volo
+{data.tranningDescription}
       </h1>
 
    
@@ -59,12 +64,26 @@ que sunto occuptatur sita volut volo
 
       </Section>
      
-<Section1/>
+<Section1  data={data}/>
  
       <NewsLetter/>
        
       <Footer/>
     
     </>
-  );
-}
+     )}
+     </>
+     );
+   }
+
+
+   _id: "60533086ab3b9e57a38706c5"
+tranningAvatar: "https://res.cloudinary.com/dzcmadjl1/image/upload/v1617807988/Airjaldi/hemslhhpi75uglddh3q2.jpg"
+tranningDescription: "Ad quaturibus sitio officae eossima dolo-\nrer umquame nullorem. Nimolup tamusci\naditasi doluptae conseque dis sam hili-\nbusamus, voluptas il ime pe doluptatem\nipsam, secabo.kjkhjk"
+tranningData: "Anis dernati ute occae. On pe dessimus quaesedic temquam fugit\naudaecus a de volupta prate autet aut eat adi accullest velentur mi,\ncon rae eiciandel id ut aut erit volupti reruptatis endandit omnis\neatem accumquam expe escias et voluptatem eseque peruptatum\nveliam nis eatem earum volupti squatur sintur."
+SectionHead: "LOREM IPSU"
+SectionData: "Erum volorec epudae. Os delit alibusam, si core vellaborum est, et lab id quatis consequo volo-\nres tenduci enturia dolorporem nimolum voluptiunt. Bus dolestrum quos doluptat qui beaquam\nrehentum, nos maionsequis si rest eum a sam la dicium rae sam velenda neceat explicae dolorum\nfugit, tem autectet, ut lab ium quame estiosa autati cume id qui berfere prem suntibu sapicim co-\nribus volupta cuptaectinus quis abo. Saepe soluptium elignat am, ea qui doluptaquid et a volorio\nodignis ut od entiissi nimi, core volupta temporit aspereriandi derciate inctatur, qui as accaesto ea\nnonsequasit ra accatus amusame vendebitiae vel is incturi amendit qui con nus dolorendi offici\nberatecerum litate vent illa ento cum estinture sequi tota conseniendi re, officae maximaximil ilis\nvendis es aut ut ea nest asimporro que nimagnam nos dolut quias rat."
+Section2img: "https://res.cloudinary.com/dzcmadjl1/image/upload/v1617808284/Airjaldi/kqyutqonj25kxh0b39ah.jpg"
+Section2Data: "“Our village is in a very remote place\nadn we didn’t have any internet\nfacilities before AirJaldi arrived here.\nNow, we no longer need to commute\n70-80 Km to get connected.”dd"
+__v: 0
+Section2Name: "Piyush Manviya, Private User, Katkumbh, Maharashtra"
