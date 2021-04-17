0defcbe0d81da9367bb005b06f6849fe09fd1559
Pankaj <pankaj.k@amakein.com>
2021-04-17 16:46:24 +0530

    dsds

--- pages/contact.js
+++ pages/contact.js
@@ -2,9 +2,14 @@ import React from 'react'
 import Contact from '../componets/layout/Contact/contact';
 import  Footer  from '../componets/layout/Footer/Footer';
 import Navbar from '../componets/layout/Navbar/BlueNavbar';
+import Head from 'next/head'
 function contact() {
     return (
         <div>
+                 <Head>
+        <title>Conatct - AirJaldi</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
              <Navbar/>
             <Contact/>
             <Footer/>
