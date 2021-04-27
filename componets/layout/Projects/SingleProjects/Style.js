d4ec78e30027e4c470658b9dcf6eea3a1c559de1
Pankaj <pankaj.k@amakein.com>
2021-04-27 21:07:51 +0530

    sdd

--- componets/layout/Projects/SingleProjects/Style.js
+++ componets/layout/Projects/SingleProjects/Style.js
@@ -239,21 +239,38 @@ span {
    width:100%;
    height:100%;
    .flex {
-    margin-top: 3%;
-     width:90%;
-     height:60%;
-    
-     .single-post{
-         display:flex;
+     margin-top:12vh;
+    width:100%;
+     height:95%;
+     
+     align-items:center;
+     display:flex;
+         
          justify-content:center;
           flex-direction:column;
-          width:100%;
+          align-items:center;
+     .single-post{
+         display:flex;
+         
+         justify-content:center;
+          flex-direction:column;
+          align-items:center;
+          width:95%;
           height:100%;
           .head {
               text-align:left;
-              font-size:2.5rem;
+              width:100%;
+             
               font-weight:1000;
           }
+          .category {
+            text-align:left;
+              width:100%;
+             
+              
+          }
+          .data {
+              width:100%;
           figure {
               width:100%;
           
@@ -263,6 +280,7 @@ span {
                 width:100%;
               }
           }
+          }
           p {
             text-align:left;
               font-size:1.2rem;
@@ -270,6 +288,10 @@ span {
               width:100%;
               margin-top:0.7rem;
               margin-bottom:0.5rem;
+              line-height:1.3;
+          }
+          h2 {
+            font-weight:900;
           }
           h1 {
               margin-top:2rem;
@@ -283,13 +305,75 @@ span {
               margin-bottom:0.5rem;
           }
      }
-
      .section {
+         margin-top:2rem;
          display:flex;
          justify-content:space-between;
+          flex-direction:column;
          align-items:center;
          width:100%;
-         height:30rem;
+       
+         
+         .left {
+             display:flex;
+             justify-content:center;
+             width:95%;
+             height:29rem;
+             height:100%;
+             img {
+                 width:100%;
+                 height:100%;
+             }
+         }
+
+         .right {
+            width:95%;
+            height:100%;
+            height:29rem;
+            display:flex;
+            justify-content:center;
+            flex-direction:column;
+            align-items:center;
+            .text {
+                width:100%;
+                height:90%;
+                display:flex;
+                justify-content:center;
+            
+                flex-direction:column;
+                h2 {
+                    width:100%;
+                    font-size:2rem;
+                    text-align:center;
+                    
+                }
+                span {
+                  text-align:center;
+                    margin-top:1rem;
+                    font-size:1.3rem;
+                }
+              
+            }
+            .link {
+               
+                width:90%;
+                height:10%;
+                display:flex;
+                justify-content:flex-start;
+                align-items:center;
+                color:${props => props.theme.colors.blue};
+                
+                i {
+  font-size:1.2rem;
+}
+span {
+    margin-left:0.5rem;
+  font-size:1.2rem;
+}
+
+                
+            }
+        }
      }
    }
    .btn {
