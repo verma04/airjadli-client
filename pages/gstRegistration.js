ff56d8095ad27134246ff3ff87386e8b39afc9f8
Pankaj <pankaj.k@amakein.com>
2021-06-03 13:06:53 +0530

    dsds

---
+++ pages/gstRegistration.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import GstRegistration from '../componets/Links/gstRegistration'
+import Head from 'next/head';
+function gstRegistration() {
+    return (
+        <div>
+            <GstRegistration/>
+        </div>
+    )
+}
+
+export default gstRegistration
---
+++ pages/privacyPolicy.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PrivacyPolicy from '../componets/Links/privacyPolicy'
+import Head from 'next/head';
+function privacyPolicy() {
+    return (
+        <div>
+                 <Head>
+    <title>Parental Controls - AirJaldi</title>
+  </Head>
+            <PrivacyPolicy/>
+        </div>
+    )
+}
+
+export default privacyPolicy
